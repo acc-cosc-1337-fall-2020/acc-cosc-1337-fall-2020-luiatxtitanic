57e7d8884969d1028617408ec7e30a2f6700cf13
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-09-21 08:00:43 -0500

    hw 0 corrections

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -7,7 +7,7 @@ using std::cout;  using std::cin; using std::endl;
 int main()
 
 {
-int num1, num2, result;
+int num1, num2=4, result;
 cout<<"This program multiplies two numbers. \n";
 
 //Get numbers from user
@@ -15,12 +15,14 @@ cout<<"Enter a number: ";//text to display
 cin>>num1;//capture number from user
 cout<<"You entered: "<< num1 << endl;
 
-cout<<"Enter a second number to multiply: ";
-cin>>num2;
-cout<<"You entered: " << num2 << endl;
+
+
 //Display product
 
-result = num1 * num2;
+result = multiply_numbers(num1);
+cout<<"The product equals " << result << endl;
+
+result = multiply_numbers(num2);
 cout<<"The product equals " << result << endl;
 return 0;
 }
