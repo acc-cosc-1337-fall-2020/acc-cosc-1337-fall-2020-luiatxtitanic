22cda35783f5e599a51c500adb2adc45002d13aa
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-10-28 15:12:40 -0500

    tictactoe1 grading

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,24 @@
+#include<iostream>
+#include<string>
+#include "tic_tac_toe.h"
 
 int main() 
 {
+	TicTacToe game;
+
+	std::string player;
+	std::cout<<"Enter X or 0: ";
+	std::cin>>player;
+
+	do
+	{
+		int position;
+		std::cout<<"Enter position 1 to 9: ";
+		std::cin>>position;
+		game.mark_board(position);
+		game.display_board();
+
+	}while(game.game_over() == false);
 
 	return 0;
 }
